de334ed88801a146773d87462a54ff8e517a220b
jincheng.jcs <jincheng.jcs@gmail.com>
2015-06-09 08:46:12 +0800

    karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,66 @@
-'use strict';
+// Karma configuration
+// Generated on Tue Jun 09 2015 08:32:50 GMT+0800 (中国标准时间)
 
-var src = './src',
-    srcFiles = src + '/**/*.js',
-    test = './test',
-    testFiles = test + '/**/*.test.js';
 module.exports = function(config) {
-    config.set({
-		basePath: './',
-        frameworks: ['mocha'],
-        files: [
-			{pattern: './node_modules/chai/chai.js', include: true},
-			srcFiles,
-			testFiles
-		],
-        client: {
-            mocha: {
-                reporter: 'spec'
-            }
-        },
-        preprocessors: {
-			srcFiles: ['babel'],
-			testFiles: ['babel']
-        },
-		'babelPreprocessor': {
-			// options go here
-		},
-		plugins: [
-			'karma-mocha'
-		]
-    })
-}
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/**/*.js',
+      'test/**/*.test.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox', 'Safari', 'IE', 'PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
